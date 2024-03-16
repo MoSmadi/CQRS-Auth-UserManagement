c6c4e465c0af280c7b0b082b4bba99acc85096b6
MoSmadi <msmadi@restaurnt365.com>
2024-03-16 17:27:59 +0200

    domain-validation : Add error class with some main shared errors

---
+++ BaseUserManagement/BaseUserManagement.Domain/Validations/Constants/Errors.cs
@@ -0,0 +1,7 @@
+namespace BaseUserManagement.Domain.Validations.Constants;
+
+public static class Errors
+{
+    public const string NullValueCode = "Error.NullValue";
+    public const string NullValueMessage = "The specified result value is null.";
+}
---
+++ BaseUserManagement/BaseUserManagement.Domain/Validations/Error.cs
@@ -0,0 +1,14 @@
+namespace BaseUserManagement.Domain.Validations;
+
+public class Error
+{
+    public string Code { get; }
+
+    public string Message { get; }
+    
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+}
