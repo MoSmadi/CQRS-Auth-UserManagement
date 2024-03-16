a13564c50db867e63a54f557a5fbb206665af754
MoSmadi <msmadi@restaurnt365.com>
2024-03-16 18:20:29 +0200

    validation-handler : Add validation result, to handle the failure results and errors when returning it, It provides a common error for general validation failures and allows holding specific error details.

--- BaseUserManagement/BaseUserManagement.Domain/Validations/Constants/Errors.cs
+++ BaseUserManagement/BaseUserManagement.Domain/Validations/Constants/Errors.cs
@@ -4,4 +4,7 @@ public static class Errors
 {
     public const string NullValueCode = "Error.NullValue";
     public const string NullValueMessage = "The specified result value is null.";
+    
+    public const string ValidationErrorCode = "ValidationError";
+    public const string ValidationErrorMessage = "A validation problem occurred.";
 }
