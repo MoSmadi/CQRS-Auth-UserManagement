42fb5c9fb269e4f865f1d2d3dc886c2da48d38a6
MoSmadi <msmadi@restaurnt365.com>
2024-03-16 17:31:26 +0200

    domain-validation : Add IEquatable and the needed operations for the errors with some summary

--- BaseUserManagement/BaseUserManagement.Domain/Validations/Constants/SharedErrors.cs
+++
@@ -1,8 +0,0 @@
-namespace BaseUserManagement.Domain.Validations.Constants;
-
-public class SharedErrors
-{
-    public static readonly Error None = new(string.Empty, string.Empty);
-    
-    public static readonly Error NullValue = new(Errors.NullValueCode, Errors.NullValueMessage);
-}
