76d54d028e99224055a702c30db007bda96513f0
MoSmadi <msmadi@restaurnt365.com>
2024-03-20 00:54:44 +0200

    add-user - Add user repository and register it

--- BaseUserManagement/BaseUserManagement.Infrastructure/ServiceCollectionExtensions.cs
+++ BaseUserManagement/BaseUserManagement.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,3 +1,5 @@
+using BaseUserManagement.Domain.Users.Repositories;
+using BaseUserManagement.Infrastructure.Users.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,11 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructureConfigurations(this IServiceCollection services, IConfiguration configuration)
     {
-        return services;
+        return services
+            .AddRepositories();
     }
+    
+    private static IServiceCollection AddRepositories(this IServiceCollection services) =>
+        services
+            .AddScoped<IUserRepository, UserRepository>();
 }
