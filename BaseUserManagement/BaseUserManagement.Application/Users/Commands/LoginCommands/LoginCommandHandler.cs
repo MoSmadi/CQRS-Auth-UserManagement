971be1184b5e0c679d69a21048a77ae87bb67b10
MoSmadi <msmadi@restaurnt365.com>
2024-03-20 01:05:58 +0200

    add-user - Add user constants and implement a logic to get the token based on real scenario and update the handler

--- BaseUserManagement/BaseUserManagement.Application/Users/Commands/LoginCommands/LoginCommandHandler.cs
+++ BaseUserManagement/BaseUserManagement.Application/Users/Commands/LoginCommands/LoginCommandHandler.cs
@@ -1,14 +1,30 @@
 using BaseUserManagement.Application.Abstractions.Messaging;
 using BaseUserManagement.Application.Users.Models.Responses;
+using BaseUserManagement.Domain.Users.Services;
 using BaseUserManagement.Domain.Validations;
 
 namespace BaseUserManagement.Application.Users.Commands.LoginCommands;
 
 public class LoginCommandHandler : ICommandHandler<LoginCommand, LoginResponse>
 {
+    private readonly IUserService _userService;
+
+    public LoginCommandHandler(IUserService userService)
+    {
+        _userService = userService;
+    }
+
     public async Task<Result<LoginResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
     {
-        var response = new LoginResponse("token");
-        return Result.Success(response);
+        var userResult = await _userService.ValidateLoginCredentialsAsync(request.UserName, request.Password, cancellationToken);
+
+        if (userResult.IsFailure)
+        {
+            return Result.Failure<LoginResponse>(userResult.Error);
+        }
+        
+        var userToken = new LoginResponse("Token");
+        
+        return Result.Success(userToken);
     }
 }
--- BaseUserManagement/BaseUserManagement.Domain/Users/Services/UserService.cs
+++ BaseUserManagement/BaseUserManagement.Domain/Users/Services/UserService.cs
@@ -1,12 +1,36 @@
+using BaseUserManagement.Domain.Users.Constants;
+using BaseUserManagement.Domain.Users.Extensions;
 using BaseUserManagement.Domain.Users.Models;
+using BaseUserManagement.Domain.Users.Repositories;
 using BaseUserManagement.Domain.Validations;
 
 namespace BaseUserManagement.Domain.Users.Services;
 
 public class UserService : IUserService
 {
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
     public async Task<Result<User>> ValidateLoginCredentialsAsync(string username, string password, CancellationToken cancellationToken)
     {
-        return Result.Success(new User("username", "password", "email", "firstName", "lastName"));
+        var user = await _userRepository.GetUserByUsernameAsync(username, cancellationToken);
+        
+        if (user is null)
+        {
+            return Result.Failure<User>(UserConstants.InvalidUsername);
+        }
+        
+        var hashedPassword = password.HashPassword();
+        
+        if (user.HashedPassword != hashedPassword)
+        {
+            return Result.Failure<User>(UserConstants.InvalidCredentials);
+        }
+        
+        return Result.Success(user);
     }
 }
