accf12baf60d88b0e2714a56cc574391b41a9978
MoSmadi <msmadi@restaurnt365.com>
2024-03-19 23:49:30 +0200

    add-messaging : Add the command and its handler base implementation

---
+++ BaseUserManagement/BaseUserManagement.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using BaseUserManagement.Domain.Validations;
+using MediatR;
+
+namespace BaseUserManagement.Application.Abstractions.Messaging;
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+{
+}
---
+++ BaseUserManagement/BaseUserManagement.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using BaseUserManagement.Domain.Validations;
+using MediatR;
+
+namespace BaseUserManagement.Application.Abstractions.Messaging;
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+}
