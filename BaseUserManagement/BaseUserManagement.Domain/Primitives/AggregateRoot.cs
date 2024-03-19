ac984c73394a6f0bb49341695f80cf3b8e44accc
MoSmadi <msmadi@restaurnt365.com>
2024-03-19 23:32:29 +0200

    add-primitives : Add entity and value object base classes

---
+++ BaseUserManagement/BaseUserManagement.Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿namespace BaseUserManagement.Domain.Primitives;
+
+public abstract class AggregateRoot : Entity
+{
+    protected AggregateRoot(Guid id) : base(id)
+    {
+    }
+
+    protected AggregateRoot()
+    {
+    }
+}
