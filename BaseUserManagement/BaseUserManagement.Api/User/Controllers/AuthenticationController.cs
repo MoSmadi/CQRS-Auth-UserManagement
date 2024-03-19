7bfad4e0bf5e127377e68efc8359b10c0141f1db
MoSmadi <msmadi@restaurnt365.com>
2024-03-20 00:20:08 +0200

    add-user - Add login endpoint that should return the token

--- BaseUserManagement/BaseUserManagement.Api/User/Controllers/AuthenticationController.cs
+++ BaseUserManagement/BaseUserManagement.Api/User/Controllers/AuthenticationController.cs
@@ -1,4 +1,7 @@
 using BaseUserManagement.Api.Abstractions;
+using BaseUserManagement.Application.Users.Commands.LoginCommands;
+using BaseUserManagement.Application.Users.Models.Requests;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BaseUserManagement.Api.User.Controllers;
@@ -6,4 +9,20 @@ namespace BaseUserManagement.Api.User.Controllers;
 [Route("api/user")]
 public sealed class AuthenticationController : ApiController
 {
+    private readonly ISender _sender;
+
+    public AuthenticationController(ISender sender)
+    {
+        _sender = sender;
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginMember([FromBody] LoginRequest request, CancellationToken cancellationToken)
+    {
+        var command = new LoginCommand(request.UserName, request.Password);
+
+        var tokenResult = await _sender.Send(command, cancellationToken);
+
+        return tokenResult.IsFailure ? HandleFailure(tokenResult) : Ok(tokenResult.Value);
+    }
 }
