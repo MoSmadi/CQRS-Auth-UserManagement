96810c328daf0bfd6301570bba850968db98089f
MoSmadi <msmadi@restaurnt365.com>
2024-03-20 00:41:14 +0200

    add-user - Return the token as an object

--- BaseUserManagement/BaseUserManagement.Application/Users/Commands/LoginCommands/LoginCommand.cs
+++ BaseUserManagement/BaseUserManagement.Application/Users/Commands/LoginCommands/LoginCommand.cs
@@ -1,5 +1,6 @@
 using BaseUserManagement.Application.Abstractions.Messaging;
+using BaseUserManagement.Application.Users.Models.Responses;
 
 namespace BaseUserManagement.Application.Users.Commands.LoginCommands;
 
-public record LoginCommand(string UserName, string Password) : ICommand<string>;
+public record LoginCommand(string UserName, string Password) : ICommand<LoginResponse>;
--- BaseUserManagement/BaseUserManagement.Application/Users/Commands/LoginCommands/LoginCommandHandler.cs
+++ BaseUserManagement/BaseUserManagement.Application/Users/Commands/LoginCommands/LoginCommandHandler.cs
@@ -1,12 +1,14 @@
 using BaseUserManagement.Application.Abstractions.Messaging;
+using BaseUserManagement.Application.Users.Models.Responses;
 using BaseUserManagement.Domain.Validations;
 
 namespace BaseUserManagement.Application.Users.Commands.LoginCommands;
 
-public class LoginCommandHandler : ICommandHandler<LoginCommand, string>
+public class LoginCommandHandler : ICommandHandler<LoginCommand, LoginResponse>
 {
-    public async Task<Result<string>> Handle(LoginCommand request, CancellationToken cancellationToken)
+    public async Task<Result<LoginResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
     {
-        return Result.Success("token");
+        var response = new LoginResponse("token");
+        return Result.Success(response);
     }
 }
