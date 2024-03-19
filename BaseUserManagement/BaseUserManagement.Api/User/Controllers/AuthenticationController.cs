b4c27cc30c573fb81e1c0060db24b788f410d364
MoSmadi <msmadi@restaurnt365.com>
2024-03-20 00:19:20 +0200

    add-user - Add auth controller

---
+++ BaseUserManagement/BaseUserManagement.Api/User/Controllers/AuthenticationController.cs
@@ -0,0 +1,9 @@
+using BaseUserManagement.Api.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseUserManagement.Api.User.Controllers;
+
+[Route("api/user")]
+public sealed class AuthenticationController : ApiController
+{
+}
