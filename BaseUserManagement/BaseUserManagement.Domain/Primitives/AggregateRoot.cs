aa703eec0e47ba05371c41dc48123f7374ec2400
MoSmadi <msmadi@restaurnt365.com>
2024-03-19 23:42:59 +0200

    add-primitives : Add IDomainEvent to allow raising events using the AggregateRoot

--- BaseUserManagement/BaseUserManagement.Domain/Primitives/AggregateRoot.cs
+++ BaseUserManagement/BaseUserManagement.Domain/Primitives/AggregateRoot.cs
@@ -2,6 +2,9 @@
 
 public abstract class AggregateRoot : Entity
 {
+    // allows different domain events to be stored in the same collection
+    private readonly List<IDomainEvent> _domainEvents = new();
+
     protected AggregateRoot(Guid id) : base(id)
     {
     }
@@ -9,4 +12,10 @@ public abstract class AggregateRoot : Entity
     protected AggregateRoot()
     {
     }
+
+    public IReadOnlyCollection<IDomainEvent> GetDomainEvents() => _domainEvents.ToList();
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
 }
