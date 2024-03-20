3381093703ce02f4e487f695c5cb3283d9b355c2
MoSmadi <msmadi@restaurnt365.com>
2024-03-20 23:44:57 +0200

    add-jwt : Add jwt settings and its configuration and register the provider

--- BaseUserManagement/BaseUserManagement.Infrastructure/ServiceCollectionExtensions.cs
+++ BaseUserManagement/BaseUserManagement.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
+using BaseUserManagement.Domain.Authentication;
 using BaseUserManagement.Domain.Users.Repositories;
+using BaseUserManagement.Infrastructure.Authentication;
+using BaseUserManagement.Infrastructure.Authentication.JwtSetup;
+using BaseUserManagement.Infrastructure.Authentication.Options;
 using BaseUserManagement.Infrastructure.Users.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,10 +14,21 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddInfrastructureConfigurations(this IServiceCollection services, IConfiguration configuration)
     {
         return services
-            .AddRepositories();
+                .AddRepositories()
+                .AddProviders()
+                .AddAJwtOptions(configuration);
     }
     
     private static IServiceCollection AddRepositories(this IServiceCollection services) =>
         services
             .AddScoped<IUserRepository, UserRepository>();
+    
+    private static IServiceCollection AddProviders(this IServiceCollection services) =>
+        services
+            .AddScoped<IJwtProvider, JwtProvider>();
+
+    private static IServiceCollection AddAJwtOptions(this IServiceCollection services, IConfiguration configuration) =>
+        services
+            .Configure<JwtOptions>(configuration.GetSection("JwtSettings"))
+            .ConfigureOptions<JwtOptionsSetup>();
 }
