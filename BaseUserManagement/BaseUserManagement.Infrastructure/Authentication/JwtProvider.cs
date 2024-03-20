cbde248083d89d9619e9b8dbea5f2a3479ac2230
MoSmadi <msmadi@restaurnt365.com>
2024-03-20 23:50:41 +0200

    add-jwt : Add GenerateJwtToken implementation and store user id in Sub

--- BaseUserManagement/BaseUserManagement.Infrastructure/Authentication/JwtProvider.cs
+++ BaseUserManagement/BaseUserManagement.Infrastructure/Authentication/JwtProvider.cs
@@ -1,12 +1,46 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using BaseUserManagement.Domain.Authentication;
 using BaseUserManagement.Domain.Users.Models;
+using BaseUserManagement.Infrastructure.Authentication.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace BaseUserManagement.Infrastructure.Authentication;
 
 public class JwtProvider : IJwtProvider
 {
+    private readonly JwtOptions _options;
+    
+    public JwtProvider(IOptions<JwtOptions> options)
+    {
+        _options = options.Value;
+    }
+    
     public string GenerateJwtToken(User user)
     {
-        return String.Empty;
+        var claims = new Claim[]
+        {
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.UniqueName, user.Username)
+        };
+        
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey!)),
+            SecurityAlgorithms.HmacSha256);
+        
+        var token = new JwtSecurityToken(
+            _options.Issuer,
+            _options.Audience,
+            claims,
+            null,
+            DateTime.UtcNow.AddHours(1),
+            signingCredentials);
+        
+        var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+        
+        return tokenValue;
     }
 }
